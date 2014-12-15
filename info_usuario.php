4e4a77162721141a88ad06386e3bde82ec89ca8c
EnriqueCruzado <EnriqueCruzado96@live.com>
2014-12-15 16:53:23 -0400

    pagina de login

--- info_usuario.php
+++ info_usuario.php
@@ -4,7 +4,7 @@
   <div class="form-group">
     <label for="nombres" class="col-sm-3 control-label">Nombres</label>
     <div class="col-sm-9">
-      <input type="text" class="form-control" id="nombres" name="nombres" placeholder="Nombres" required
+      <input type="text" class="form-control" id="nombres" name="nombres" placeholder="Nombres" required>
       <?php if(isset($datos_encargado['nombres'])) print 'value="'.$datos_encargado['nombres'].'"'?>
     </div>
   </div>
@@ -12,7 +12,7 @@
   <div class="form-group">
     <label for="apellidos" class="col-sm-3 control-label">Apellidos</label>
     <div class="col-sm-9">
-      <input type="text" class="form-control" id="apellidos" name="apellidos" placeholder="Apellidos" required
+      <input type="text" class="form-control" id="apellidos" name="apellidos" placeholder="Apellidos" required>
       <?php if(isset($datos_encargado['apellidos'])) print 'value="'.$datos_encargado['apellidos'].'"'?>
     </div>
   </div>
@@ -20,7 +20,7 @@
   <div class="form-group">
     <label for="username" class="col-sm-3 control-label">Nombre usuario</label>
     <div class="col-sm-9">
-      <input type="text" class="form-control" id="username" name="username" placeholder="Nombre usuario" required
+      <input type="text" class="form-control" id="username" name="username" placeholder="Nombre usuario" required>
       <?php if(isset($datos_encargado['username'])) print 'value="'.$datos_encargado['username'].'"'?>
     </div>
   </div>
@@ -28,7 +28,7 @@
   <div class="form-group">
     <label for="email" class="col-sm-3 control-label">email</label>
     <div class="col-sm-9">
-      <input type="email" class="form-control" id="email" name="email" placeholder="email" required
+      <input type="email" class="form-control" id="email" name="email" placeholder="email" required>
       <?php if(isset($datos_encargado['email'])) print 'value="'.$datos_encargado['email'].'"'?>
     </div>
   </div>
@@ -36,7 +36,7 @@
   <div class="form-group">
     <label for="email" class="col-sm-3 control-label">Tel&eacute;fono</label>
     <div class="col-sm-9">
-      <input type="tel" class="form-control" id="telefono" name="telefono" placeholder="Tel&eacute;fono"
+      <input type="tel" class="form-control" id="telefono" name="telefono" placeholder="Tel&eacute;fono">
       <?php if(isset($datos_encargado['telefono'])) print 'value="'.$datos_encargado['telefono'].'"'?>
 
     </div>
