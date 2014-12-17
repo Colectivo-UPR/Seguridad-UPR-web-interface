f5485ea2f4c0792ed90316bbac85bab3fc8ee688
EnriqueCruzado <EnriqueCruzado96@live.com>
2014-12-17 15:11:51 -0400

    Overflow de las paginas y login file

---
+++ loginaction.php
@@ -0,0 +1,8 @@
+<?php 
+require_once("funciones.php");
+$servicio= "http://54.172.3.196:8000";
+$route= "api-token-auth";
+
+// curl -X POST -H "Content-Type: application/json" -d '{"username":"prueba","password":"test"}' http://54.172.3.196:8000/api-token-auth/
+// curl -X GET -H "Authorization: Token f1aa9c78927c01617c1b0d1540bc2064375768b9" http://54.172.3.196:8000/reports/
+?>
