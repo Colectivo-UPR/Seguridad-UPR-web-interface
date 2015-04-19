c9f8986d0b653e47b7ea69dd3a8712ac46a6b350
EnriqueCruzado <EnriqueCruzado96@live.com>
2015-04-19 16:29:23 -0400

    checkboxes de tipos

--- editarusuario.php
+++ editarusuario.php
@@ -8,13 +8,32 @@ $id = $_GET['id'];
 $a = curl_put($server, $route, $id , $datos, $_SESSION['token']);
 var_dump($a);
 
-if($_POST['tipoActual'] != $_POST['tipoCambio'] && $_POST['tipoCambio'] != "NO"){
-	$rutaTipo = "staff-permissions/" . $_POST['email'];
-	$datoTipo = array("tipo"=> $_POST['tipoCambio']) ;
-	$tipo = curl_post($server, $rutaTipo, $datoTipo, $_SESSION['token']);
-	echo "TIPO: ";
-	var_dump($tipo);
+if (isset($_GET['director'])) {
+	$director = "true";
+} else {
+	$director = "false";
 }
 
-header("location: usuarios.php");
+if (isset($_GET['manager'])) {
+	$manager = "true";
+} else {
+	$manager = "false";
+}
+
+if (isset($_GET['official'])) {
+	$official = "true";
+} else {
+	$official = "false";
+}
+
+echo $director;
+echo $manager;
+echo $official;
+
+$rutaTipo = "staff-permissions/" . $_GET['email'];
+$datoTipo = array("is_director"=> $director, "is_shift_manager"=> $manager, "is_official"=> $official);
+$tipo = curl_post($server, $rutaTipo, $datoTipo, $_SESSION['token']);
+var_dump($tipo);
+
+// header("location: usuarios.php");
 ?>
