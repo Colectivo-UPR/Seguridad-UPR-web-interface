6e9e25fe5b09ef6127da692c3ab8379a6cce0714
Pablo Gilberto <gilbert_g17@hotmail.com>
2015-05-03 14:04:21 -0400

    Added: 1. "Bootstrap table" source links and scripts to "header.php" to display complaints(querellas) in "querella_list.php". 2. Custom css for "bootstrap table". 3. Code for displaying complaints(querellas) in "querella_list.php".

--- headers.php
+++ headers.php
@@ -20,7 +20,7 @@ require_once("connect.php");
         <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="js/bootstrap.js"></script>
+    <script src="js/bootstrap.min.js"></script>
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -28,6 +28,16 @@ require_once("connect.php");
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
+
+    <!-- Latest compiled and minified CSS -->
+      <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/bootstrap-table/1.7.0/bootstrap-table.min.css">
+
+    <!-- Latest compiled and minified JavaScript -->
+      <script src="//cdnjs.cloudflare.com/ajax/libs/bootstrap-table/1.7.0/bootstrap-table.min.js"></script>
+
+    <!-- Latest compiled and minified Locales -->
+      <script src="//cdnjs.cloudflare.com/ajax/libs/bootstrap-table/1.7.0/locale/bootstrap-table-es-AR.min.js"></script>
+      
   </head>
 
 <!-- topbar y sidebar -->
